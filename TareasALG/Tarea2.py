df4610f7efd1a5850ec37b4ff504b6aaadd4b8cd
PereMarco <pere.marcogarcia@gmail.com>
2020-12-08 12:20:17 +0100

    Corrección de tareas
    Tarea 1, 2 y 3

--- TareasALG/Tarea2.py
+++ TareasALG/Tarea2.py
@@ -1,29 +1,27 @@
 import numpy as np
 
-def DLR(a, b):
+def dp_levenshtein_backwards(a, b,thr=2**31):
     c = len(a) + 1
     f = len(b) + 1
     n = max(c,f)
-    M = np.zeros((n, n))
-    for i in range(1, n):
-        M[i, 0] = i
-        M[0, i] = i
-
+    V1 = np.zeros(n)
+    V2 = np.zeros(n)
+    for i in range(n):
+        V1[i] = i
+    
     for col in range(1, c):
+        V2[0] = col
         for fil in range(1, f):
             cost = not a[col - 1] == b[fil - 1]
-            M[col, fil] = min(M[col - 1, fil] + 1,
-                              M[col, fil - 1] + 1,
-                              M[col - 1, fil - 1] + cost,
-                              M[col - 2, fil - 2] + 1 if a[col - 1] == b[fil - 2] and b[fil - 1] == a[col - 2] else 2**32)
+            V2[fil] = min(V1[fil] + 1,
+                          V2[fil - 1] + 1,
+                          V1[fil - 1] + cost)
+        V1, V2 = V2, V1
+        if min(V1) > thr: return thr + 1
+    if V1[f - 1] > thr: return thr + 1
+    return V1[f - 1]
 
-    # for fil in range(f):
-    #     print(M[fil]) 
-
-    return M[c - 1, f - 1]
-
-
-def DLROpt(a, b):
+def dp_restricted_damerau_backwards(a, b, thr=2**31):
     c = len(a) + 1
     f = len(b) + 1
     n = max(c,f)
@@ -40,8 +38,8 @@ def DLROpt(a, b):
             V2[i] = min(V1[i] + 1,
                         V2[i - 1] + 1,
                         V1[i - 1] + cost)
-    # print(V1)
-    # print(V2)
+    
+    if min(V2) > thr: return thr + 1
     for col in range(2, c):
         V3[0] = col
         for fil in range(1, f):
@@ -51,10 +49,50 @@ def DLROpt(a, b):
                               V2[fil - 1] + cost,
                               V1[fil - 2] + 1 if a[col - 1] == b[fil - 2] and b[fil - 1] == a[col - 2] else 2**32)
         V1, V2, V3 = V2, V3, V1
-        # print(V2)
-
+        if min(V2) > thr: return thr + 1
+    if V2[f - 1] > thr: return thr + 1
     return V2[f - 1]
 
-if __name__ == "__main__":
-    print(DLR("ayz","abc"))
-    print(DLROpt("ayz","abc"))
+def dp_intermediate_damerau_backwards(a, b, thr = 2**31):
+    inf = 2**32
+    c = len(a) + 1
+    f = len(b) + 1
+    n = max(c,f)
+    V1 = np.zeros(n)
+    V2 = np.zeros(n)
+    V3 = np.zeros(n)
+    for i in range(f):
+        V1[i] = i
+        
+    if len(a) > 1:
+        V2[0] = 1
+        for i in range(1, f):
+            cost = not a[0] == b[i - 1]
+            V2[i] = min(V1[i] + 1,
+                        V2[i - 1] + 1,
+                        V1[i - 1] + cost)
+    if min(V2) > thr: return thr + 1
+    if len(a) > 2:
+        V3[0] = 2
+        for i in range(1,f):
+            cost = not a[1] == b[i - 1]
+            V3[i] = min(V2[i] + 1,
+                                V3[i - 1] + 1,
+                                V2[i - 1] + cost,
+                                V1[i - 2] + 1 if a[1] == b[i - 2] and b[i - 1] == a[0] else inf)
+    V1, V2, V3 = V2, V3, V1
+    if min(V2) > thr: return thr + 1
+    for col in range(3, c):
+        V3[0] = col
+        for fil in range(1, f):
+            cost = not a[col - 1] == b[fil - 1]
+            V3[fil] = min(V2[fil] + 1,
+                              V3[fil - 1] + 1,
+                              V2[fil - 1] + cost,
+                              V1[fil - 2] + 1 if a[col - 1] == b[fil - 2] and b[fil - 1] == a[col - 3] else inf,
+                              V1[fil - 2] + 1 if a[col - 1] == b[fil - 3] and b[fil - 1] == a[col - 2] else inf,
+                              V1[fil - 2] + 1 if a[col - 1] == b[fil - 2] and b[fil - 1] == a[col - 2] else inf)
+        V1, V2, V3 = V2, V3, V1
+        if min(V2) > thr: return thr + 1
+    if V2[f - 1] > thr: return thr + 1
+    return V2[f - 1]
